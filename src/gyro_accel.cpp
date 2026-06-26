ed4f801c08c9fdae3b3c55bf3a2ea00f1cce25eb
Yasmine Benhaddou <benhaddou42@gmail.com>
2026-06-26 11:33:21 +0100

    V1

--- src/gyro_accel.cpp
+++ src/gyro_accel.cpp
@@ -6,14 +6,22 @@
 #include "accel_gyro.h"
 
 Adafruit_MPU6050 mpu;
+
+const float ALPHA = 0.98;
+const float ALPHA_GYRO = 0.8;
+
 float gyroX_offset = 0;
+float angle_offset = 0;
 float angle_filtre = 0;
+float gyro_filtre = 0;
+
 unsigned long last_time = 0;
 
 void initMPU()
 {
     Serial.begin(115200);
     Wire.begin(21, 22);
+
     if (!mpu.begin())
     {
         Serial.println("MPU6050 non detecte !");
@@ -22,28 +30,50 @@ void initMPU()
             delay(10);
         }
     }
+
     Serial.println("MPU6050 detecte");
+
     mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
     mpu.setGyroRange(MPU6050_RANGE_500_DEG);
     mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
     last_time = millis();
 }
 
 void calibrerGyro()
 {
     sensors_event_t accel, gyro, temp;
-    float somme = 0;
-    Serial.println("Calibration gyro... ne pas bouger le robot");
+
+    float somme_gyro = 0;
+    float somme_angle = 0;
+
+    Serial.println("Calibration... garder le robot dans sa position initiale");
+
     for (int i = 0; i < 500; i++)
     {
         mpu.getEvent(&accel, &gyro, &temp);
-        somme += gyro.gyro.x * 180.0 / PI;
+
+        float ay = accel.acceleration.y;
+        float az = accel.acceleration.z;
+
+        somme_gyro += gyro.gyro.x * 180.0 / PI;
+        somme_angle += atan2(ay, az) * 180.0 / PI;
+
         delay(2);
     }
-    gyroX_offset = somme / 500.0;
+
+    gyroX_offset = somme_gyro / 500.0;
+    angle_offset = somme_angle / 500.0;
+
+    angle_filtre = 0;
+    gyro_filtre = 0;
+    last_time = millis();
+
     Serial.print("Offset GyroX = ");
     Serial.println(gyroX_offset);
-    last_time = millis();
+
+    Serial.print("Angle reference = ");
+    Serial.println(angle_offset);
 }
 
 float readAngleFiltre()
@@ -55,29 +85,30 @@ float readAngleFiltre()
     float dt = (current_time - last_time) / 1000.0;
     last_time = current_time;
 
-    float ax = accel.acceleration.x;
+    if (dt <= 0)
+    {
+        dt = 0.001;
+    }
+
     float ay = accel.acceleration.y;
     float az = accel.acceleration.z;
 
-    // ✅ CORRECTION 1 : atan2(ay, az) au lieu de atan2(ax, sqrt(ay*ay + az*az))
-    // ❌ AVANT : float angle_accel = atan2(ax, sqrt(ay * ay + az * az)) * 180.0 / PI;
     float angle_accel = atan2(ay, az) * 180.0 / PI;
+    angle_accel = angle_accel - angle_offset;
 
     float gyro_x_deg = gyro.gyro.x * 180.0 / PI;
     float gyro_x_corrige = gyro_x_deg - gyroX_offset;
 
-    angle_filtre = 0.98 * (angle_filtre + gyro_x_corrige * dt)
-                 + 0.02 * angle_accel;
+    gyro_filtre = ALPHA_GYRO * gyro_filtre + (1.0 - ALPHA_GYRO) * gyro_x_corrige;
 
-    Serial.print(-30);
+    angle_filtre = ALPHA * (angle_filtre + gyro_filtre * dt)
+                 + (1.0 - ALPHA) * angle_accel;
+
+    Serial.print(-90);
     Serial.print(" ");
-    Serial.print(30);
+    Serial.print(90);
     Serial.print(" ");
     Serial.println(angle_filtre);
 
-    // ✅ CORRECTION 2 : delay(50) supprimé d'ici
-    // ❌ AVANT : delay(50) ici faussait le calcul de dt
-    
     return angle_filtre;
 }
-
