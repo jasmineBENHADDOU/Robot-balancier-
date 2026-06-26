82463a963b8f547ef7976d4818eb78b35eabe1ec
tmanelle <manelle.tahiri@ynov.com>
2026-06-26 11:56:58 +0200

    Modification du main pour ajouter le calcul de l'erreur e

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-
 #include "accel_gyro.h"
+#include <Arduino.h>
+
+const float CONSIGNE = 0.0; // si le robot est droit alors l'angle de référence = 0°
 
 void setup()
 {
@@ -12,4 +14,14 @@ void loop()
     float angle = readAngleFiltre();
 
     // erreur = consigne - angle;
+    float erreur = CONSIGNE - angle;
+
+    // Afficher l'erreur en temps réel
+    Serial.print("Angle");
+    Serial.print(angle);
+    Serial.print(" ");
+    Serial.print("Erreur");
+    Serial.println(erreur);
+
+    delay(10);
 }
